6ade7f9d157467144d5daf84a7f4f3bd8fa27c03
Desmond <adabedesmond@gmail.com>
2023-03-13 20:48:43 +0000

    modified code

--- widgets/chart.dart
+++ widgets/chart.dart
@@ -38,7 +38,6 @@ class Chart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 1,
-      
       margin:const EdgeInsets.all(20),
       child: Row(
         children: groupedTransactions.map((element){

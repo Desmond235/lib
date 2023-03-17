f2aff6fe0e1c804b1912096e9fee2269dfe9e085
Adabe Desmond <adabedesmond@gmail.com>
2023-03-17 09:08:58 +0000

    modified content

--- widgets/chart_bar.dart
+++ widgets/chart_bar.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+
 class ChartBar extends StatelessWidget {
-   const ChartBar({Key? key,  required this.label,required this.amountPer, required this.amountSpent}) : super(key: key);
+  const ChartBar(
+      {Key? key,
+      required this.label,
+      required this.amountPer,
+      required this.amountSpent})
+      : super(key: key);
   final String label;
   final double amountSpent;
   final double amountPer;
@@ -9,7 +15,9 @@ class ChartBar extends StatelessWidget {
     return Column(
       children: [
         FittedBox(child: Text('\$${amountSpent.toStringAsFixed(0)}')),
-        const SizedBox(height: 5,),
+        const SizedBox(
+          height: 5,
+        ),
         // ignore: sized_box_for_whitespace
         Container(
           height: 60,
@@ -17,25 +25,25 @@ class ChartBar extends StatelessWidget {
           child: Stack(
             children: [
               Container(
-                decoration: BoxDecoration( 
-                  border: Border.all(color: Colors.grey, width: 1.0),
-                  color: const Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(15)
-                ),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 1.0),
+                    color: const Color.fromRGBO(220, 220, 220, 1),
+                    borderRadius: BorderRadius.circular(15)),
               ),
               FractionallySizedBox(
-                heightFactor:amountPer ,
+                heightFactor: amountPer,
                 child: Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(15)
-                  ),
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(15)),
                 ),
               ),
             ],
           ),
         ),
-        const SizedBox(height: 5,),
+        const SizedBox(
+          height: 5,
+        ),
         Text(label)
       ],
     );
